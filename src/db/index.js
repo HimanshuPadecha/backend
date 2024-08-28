f245940d676e12b699c56a356e80a89b27f81e2c
himanshu <himanshupadecha@gmail.com>
2024-08-28 14:53:39 +0530

    reomving logs

--- src/db/index.js
+++ src/db/index.js
@@ -2,11 +2,8 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connectDb = async () => {
-    console.log(process.env.DATABASE_URL);
   try {
-    const response = await mongoose.connect(`${process.env.DATABASE_URL}/${DB_NAME}`)
-    console.log(response);
-    
+    await mongoose.connect(`${process.env.DATABASE_URL}/${DB_NAME}`)
   } catch (error) {
     console.log("Error in connection : ",error);
     process.exit(1)    
