31b2d14f8d7d390f109c240a7cb7e9d9ceabfcf3
himanshu <himanshupadecha@gmail.com>
2024-08-29 11:31:52 +0530

    commit again

--- src/db/index.js
+++ src/db/index.js
@@ -4,6 +4,8 @@ import { DB_NAME } from "../constants.js";
 const connectDb = async () => {
   try {
     await mongoose.connect(`${process.env.DATABASE_URL}/${DB_NAME}`)
+    console.log("Database is conneted");
+    
   } catch (error) {
     console.log("Error in connection : ",error);
     process.exit(1)    
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import dotenv from "dotenv"
 import connectDb from "./db/index.js";
-
+import {app} from "./app.js"
 dotenv.config({
     path:"./env"
 })
 
-connectDb()
+connectDb().then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log("The app is running on port no "+ process.env.PORT);
+    })
+}).catch(err=> console.log("Database connection error : " , err)
+)
